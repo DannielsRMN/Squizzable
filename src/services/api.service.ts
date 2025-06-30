e9c20951cb53d2c9a17f0f4d32e34cc7aa5ada16
DannielsRMN <DannielsR.MN@gmail.com>
2025-06-29 19:19:58 -0500

    Corrección de HORRORES, ay no | By Danniels

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { modulo } from "../models/modulo.model";
+import { especialidad } from "../models/especialidad.model";
 
 @Injectable({
   providedIn: "root"
 })
 
-export class ModuloService {
+export class ApiService {
     private ApiUrl = "http://127.0.0.1:8000/api/"; // URL to web api
     private httpOptions = {
         headers: new HttpHeaders({
@@ -17,6 +18,7 @@ export class ModuloService {
     constructor(private http: HttpClient) {
     }
 
+    // CRUD Modulo
     public getModulo(): Observable<modulo[]> {
         return this.http.get<modulo[]>(this.ApiUrl + 'Modulo');
     }
@@ -33,6 +35,25 @@ export class ModuloService {
     public postModulo(modulo:modulo): Observable<modulo>{
         let body = JSON.stringify(modulo);
         return this.http.post<modulo>(this.ApiUrl + 'Modulo/',body,this.httpOptions);
+    }
+
+    // CRUD Modulo
+    public getEspecialidad(): Observable<especialidad[]> {
+        return this.http.get<especialidad[]>(this.ApiUrl + 'Especialidad');
+    }
+
+    public deleteEspecialidad(id:string): Observable<void>{
+        return this.http.delete<void>(this.ApiUrl + 'Especialidad/' + id + "/");
+    }
+
+    public putEspecialidad(especialidad:especialidad): Observable<especialidad>{
+        let body = JSON.stringify(modulo);
+        return this.http.put<especialidad>(this.ApiUrl + 'Especialidad/' + especialidad.idEspecialidad + "/",body,this.httpOptions);
+    }
+
+    public postEspecialidad(especialidad:especialidad): Observable<especialidad>{
+        let body = JSON.stringify(especialidad);
+        return this.http.post<especialidad>(this.ApiUrl + 'Especialidad/',body,this.httpOptions);
     }
 
 }
