1a62337ac0b2c30a7963549a90f2415eaf4b9e64
DannielsRMN <DannielsR.MN@gmail.com>
2025-07-06 16:28:25 -0500

    Nuevas Integraciones

---
+++ src/app/crud-modulos/crud-modulos.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { MenuVisibilityService } from '../../services/visible.service';
+import { modulo } from '../../models/modulo.model';
+import { especialidad } from '../../models/especialidad.model';
+
+@Component({
+  selector: 'app-crud-modulos',
+  standalone: false,
+  templateUrl: './crud-modulos.component.html',
+  styleUrl: './crud-modulos.component.css',
+  providers: [ApiService]
+
+})
+export class CrudModulosComponent {
+
+  constructor(private api: ApiService, private menu: MenuVisibilityService) {
+      this.mostrarMenu = true;
+    }
+
+    mostrarMenu = true
+
+    ngOnInit() {
+      this.obtenerModulos();
+      this.obtenerEspecialidades();
+
+      setTimeout(() => {
+        this.menu.showMenu();
+      }, 0);
+    }
+
+    modulos: modulo[];
+    visible: boolean = false;
+    nuevoModulo: boolean = true;
+    moduloDialogo: modulo = new modulo();
+
+    dificultades: string[] = ['facil', 'intermedio', 'avanzado']
+    dif_select: string;
+
+    especialidades: especialidad[];
+    espe_select: especialidad;
+
+    abrirDialogo() {
+      this.visible = true;
+    }
+
+    obtenerModulos() {
+      this.api.getModulo().subscribe(res => {
+        this.modulos = res;
+      })
+    }
+
+    obtenerEspecialidades() {
+      this.api.getEspecialidad().subscribe(res => {
+        this.especialidades = res;
+      })
+    }
+
+    editarModulo(modulo: modulo) {
+      this.visible = true;
+      this.nuevoModulo = false;
+      this.moduloDialogo = modulo;
+      this.espe_select = this.especialidades.find(t => t.idEspecialidad === modulo.especialidad)!;
+    }
+
+    eliminarModulo(modulo: modulo) {
+      this.api.deleteModulo(modulo.idModulo.toString()).subscribe(() => {
+        this.obtenerModulos();
+      });
+    }
+
+    guardarModulo() {
+      this.moduloDialogo.especialidad = this.espe_select.idEspecialidad;
+      this.moduloDialogo.dificultad = this.dif_select;
+
+      if (this.nuevoModulo) {
+        this.api.postModulo(this.moduloDialogo).subscribe(res => {
+          this.obtenerModulos();
+        });
+      } else {
+        this.api.putModulo(this.moduloDialogo).subscribe(res => {
+          this.obtenerModulos();
+        });
+      }
+
+      this.visible = false
+    }
+
+}
--- src/app/pregunta/pregunta.component.ts
+++ src/app/pregunta/pregunta.component.ts
@@ -13,70 +13,60 @@ import { ApiService } from '../../services/api.service';
 export class PreguntaComponent {
 
 
-  constructor(private api:ApiService){}
+  constructor(private api: ApiService) { }
 
-preguntas:pregunta[];
-visible:boolean=false;
-nuevoPregunta:boolean=true;
-preguntaDialogo:pregunta= new pregunta();
-temas:tema[];
-temaSelecionado:tema;
+  preguntas: pregunta[];
+  visible: boolean = false;
+  nuevoPregunta: boolean = true;
+  preguntaDialogo: pregunta = new pregunta();
+  temas: tema[];
+  temaSelecionado: tema;
 
-obtenerPregunta(){
-  this.api.getPregunta().subscribe(res=>{
-    this.preguntas=res;
-  })
-
-
-}
-
-obtenerTema(){
-  this.comp.getTema().subscribe(res=>{
-    this.temas=res;
-  });
-}
-  ngOnInit(){
-this.obtenerPregunta();
-this.obtenerTema();
-
+  obtenerPregunta() {
+    this.api.getPregunta().subscribe(res => {
+      this.preguntas = res;
+    })
   }
 
-  editarPregunta(pregunta: pregunta){
-    this.visible=true;
-    this.nuevoPregunta=false,
-    this.preguntaDialogo=pregunta;
- 
-
-  }
-
-  eliminarPregunta(pregunta: pregunta){
-this.api.deletePregunta(pregunta.idPregunta.toString()).subscribe(()=>{
-  this.obtenerPregunta();
-})
-  
-  }
- 
-
-  abrirPegunta(){
-  this.visible=true;
-     
-  }
-
-guardarPregunta(){
- this.preguntaDialogo.tema=this.temaSelecionado.idTema;
-  if(this.nuevoPregunta){
-
-    this.api.postPregunta(this.preguntaDialogo).subscribe(res=>{
-      this.obtenerPregunta();
-    });
-  }else {
-    this.api.putPregunta(this.preguntaDialogo).subscribe(res=>{
-      this.obtenerPregunta();
+  obtenerTema() {
+    this.api.getTema().subscribe(res => {
+      this.temas = res;
     });
   }
-  this.visible=false;
-}
+  ngOnInit() {
+    this.obtenerPregunta();
+    this.obtenerTema();
+  }
+
+  editarPregunta(pregunta: pregunta) {
+    this.visible = true;
+    this.nuevoPregunta = false,
+      this.preguntaDialogo = pregunta;
+  }
+
+  eliminarPregunta(pregunta: pregunta) {
+    this.api.deletePregunta(pregunta.idPregunta.toString()).subscribe(() => {
+      this.obtenerPregunta();
+    })
+  }
+
+  abrirPegunta() {
+    this.visible = true;
+  }
+
+  guardarPregunta() {
+    this.preguntaDialogo.tema = this.temaSelecionado.idTema;
+    if (this.nuevoPregunta) {
+
+      this.api.postPregunta(this.preguntaDialogo).subscribe(res => {
+        this.obtenerPregunta();
+      });
+    } else {
+      this.api.putPregunta(this.preguntaDialogo).subscribe(res => {
+        this.obtenerPregunta();
+      });
+    }
+    this.visible = false;
+  }
 
 }
-
-
