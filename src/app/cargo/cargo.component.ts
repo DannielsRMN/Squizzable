81f7f55a90bbae08f96690a3af458d81db198f66
Jean Lopez <2022160049@udh.edu.pe>
2025-07-06 16:35:56 -0500

    Cargo

---
+++ src/app/cargo/cargo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cargo',
+  standalone: false,
+  templateUrl: './cargo.component.html',
+  styleUrl: './cargo.component.css'
+})
+export class CargoComponent {
+
+}
