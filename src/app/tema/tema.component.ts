cd81d6683cd8bdb216ed0b7be09db58ee6ebf02e
jefersonM1802 <jefripalacios1802@gmail.com>
2025-07-06 16:19:13 -0500

    tema subida de prueba

---
+++ src/app/tema/tema.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tema',
+  standalone: false,
+  templateUrl: './tema.component.html',
+  styleUrl: './tema.component.css'
+})
+export class TemaComponent {
+
+}
